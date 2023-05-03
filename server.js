03085e6f4a87011e1f80b304ff7151195f9bf63a
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-05-03 17:19:14 -0400

    link stylesheet

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ server.engine('html', es6Renderer);// rendering files
 server.set('views', 'views'); //config we are telling the computer where to look
 server.set('view engine', 'html');
 
+server.use(express.static(__dirname + '/public')); // .use is key word that will be used for middleware for every single file
 
 server.get('/', (req,res) =>{
     res.render('index'); //pulling index
