ab9c08afb857fb4233b345d6f3dabd37408fa375
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-05-03 17:04:18 -0400

    render landing page at /

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+
+//imports
 require('dotenv').config();
+const es5Render = require('express-es6-template-engine');
 const express = require('express');
+const es6Renderer = require('express-es6-template-engine');
 
 const server = express();
-
 const  PORT  = process.env.PORT || 8080;
+
+server.engine('html', es6Renderer);// rendering files 
+server.set('views', 'views'); //config we are telling the computer where to look
+server.set('view engine', 'html');
+
+
+server.get('/', (req,res) =>{
+    res.render('index'); //pulling index
+})
 
 server.get('/heartbeat', (req, res) => {
     res.json({
