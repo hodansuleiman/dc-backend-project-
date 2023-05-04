da7654f29986d5ddd592a7dc1d7f7aab79579e5d
Hodan Suleiman <suleimanhodan9@gmail.com>
2023-05-04 12:44:26 -0400

    style login view and clear inputs on submit

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('form'); // initliazing form to be element from DOM (in index it's grabbing elementbyID 'form)
+const credsContainer =form.querySelector('#credentials-container');
 
 
 //function to make data readalble which is passed in handlesubmit 
@@ -18,7 +19,22 @@ const handleSubmit = (e) => {
     //we need to make the data readiable so we use stringufy
     const stringified = stringifyFormData(data) // firing function stringify 
     console.log(stringified);
+    renderForm();
 };
 
 
+renderForm()// load function
 form.addEventListener('submit', handleSubmit); // handeling submit eventon form capturing data
+
+function renderForm() {
+        const html = `
+        <div class="input-field">
+            <input type="text" name="username" id="username" placeholder="Enter Username">
+        </div>
+        <div class="input-field">
+            <input type="password" name="password" id="password" placeholder="Enter Password">
+        </div>  
+            <input type="submit" value="LogIn">
+        `;
+        credsContainer.innerHTML = html;
+}
